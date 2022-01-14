6e791f681a8f9482c0aa606f8c8f68cac28532e5
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-01-15 01:42:10 +0530

    Moved files to functions and dist directories

---
+++ functions/server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const timestamp = require('./timestamp');
+const app = express();
+const serverless = require('serverless-http');
+const router = express.Router();
+
+// enable CORS
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// Hide X-powered-by header ;)
+app.use(function (req, res, next) {
+    res.header("X-powered-by", "Efforts, Sweat, Dedication and Desire");
+    next();
+});
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+router.get("/", function (req, res) {
+    // Removing /functions from the path of html file
+    const options = {
+        root: __dirname.replace('/functions', '')
+    };
+    res.sendFile('/dist/index.html', options);
+});
+
+
+// Simple Hello API endpoint... 
+router.get("/api/hello", function (req, res) {
+    res.json({ greeting: 'hello API' });
+});
+
+
+// Date API endpoint... 
+router.get("/api/:date?", function (req, res) {
+    let dateInput = req.params.date;
+    let currentDate = timestamp(dateInput);
+
+    if (currentDate.toString() === 'Invalid Date') {
+        res.json({
+            error: currentDate.toString()
+        });
+    }
+    else {
+        res.json({
+            unix: currentDate.getTime(),
+            utc: currentDate.toUTCString()
+        });
+    }
+});
+
+
+// Used to redirect to router
+app.use('/', router);
+
+
+// listen for requests :)
+// var listener = app.listen(process.env.PORT, function () {
+//     console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+
+module.exports.handler = serverless(app);
